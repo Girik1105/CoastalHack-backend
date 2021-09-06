82fc4e9fb04940297720eb81ce71cd0f079b290e
Girik1105 <girik1105@gmail.com>
2021-09-06 16:39:44 +0530

    Completed Community CRUD

---
+++ scripts/create.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+AUTH_ENDPOINT = 'http://127.0.0.1:8000/api/token/'
+
+user_data = {
+    'email':'girik1105@gmail.com',
+    'password':'TestPassword'
+}
+
+headers = {
+    "Content-Type": "application/json",
+}
+
+r = requests.post(AUTH_ENDPOINT, data=json.dumps(user_data), headers=headers)
+token = r.json()['access']
+
+print('Authorization call:', r.status_code)
+print('JWT TOken Recieved:', token)
+
+headers2 = {
+    "Content-Type": "application/json",
+    "Authorization": "JWT " + token,
+}
+
+post_data = {
+    'name':"New community 2",
+    'description': 'this new 3 from api call',
+    'cover':'uploads/covers/default.jpg',
+}
+
+ENDPOINT = "http://127.0.0.1:8000/api/communities/create/"
+
+r = requests.post(ENDPOINT, data=json.dumps(post_data), headers=headers2)
+print("Create Response:", r.status_code)
+# print(r.json())
